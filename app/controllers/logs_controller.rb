e37d93db82c4c38edfbbb865ba3a457d4e774f20
Anne Richardson <richardson.ae@gmail.com>
2019-03-26 22:29:04 -0500

    Add styles to 'add log' buttons

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,9 +1,17 @@
 class LogsController < ApplicationController
   def index
-    physical_therapy_sessions = current_user.physical_therapy_sessions
-    pain_logs = current_user.pain_logs
-    exercise_logs = current_user.exercise_logs
+    @logs ||= integrated_logs
+  end
 
-    @logs ||= [physical_therapy_sessions.to_a, pain_logs.to_a, exercise_logs.to_a].flatten.sort_by { |a| a[:datetime_occurred] }.reverse!
+  private
+
+  def integrated_logs
+    logs = [
+      current_user.physical_therapy_sessions.to_a,
+      current_user.pain_logs.to_a,
+      current_user.exercise_logs.to_a
+    ]
+
+    logs.flatten.sort_by { |a| a[:datetime_occurred] }.reverse!
   end
 end
