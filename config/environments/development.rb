9c7bf65f9e30cea64e33c02483bb9e4b059eb98d
Anne Richardson <richardson.ae@gmail.com>
2019-03-28 21:36:01 -0500

    Set time zone to Central

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.time_zone = 'Central Time (US & Canada)'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.time_zone = 'Central Time (US & Canada)'
 end
