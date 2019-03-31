d1bbfe3e3ad543dc7787968829ecbd77095a793d
Anne Richardson <richardson.ae@gmail.com>
2019-03-31 11:40:51 -0500

    Toggle homework

---
+++ app/assets/javascripts/logs.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let homeworkList = document.getElementById('homework-list')
+  let homeworkToggle = document.getElementById('homework-list-toggle')
+
+  homeworkToggle.addEventListener('click', function(event){
+    event.preventDefault();
+    button = event.target;
+
+    if(homeworkToggle.classList.contains('fa-caret-down')){
+      homeworkToggle.classList.add('fa-caret-right')
+      homeworkToggle.classList.remove('fa-caret-down')
+      homeworkList.classList.remove('hidden')
+    } else {
+      homeworkToggle.classList.add('fa-caret-down')
+      homeworkToggle.classList.remove('fa-caret-right')
+      homeworkList.classList.add('hidden')
+    }
+  })
+});
