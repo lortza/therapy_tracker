d51f99777e20010d071cd5876cde590e6a664df3
Anne Richardson <richardson.ae@gmail.com>
2019-04-01 22:10:32 -0500

    WIP counting in the browser with dynamic inputs

--- app/assets/javascripts/exercise_logs.js
+++ app/assets/javascripts/exercise_logs.js
@@ -1,50 +1,64 @@
-// var myEl = document.getElementsByTagName("p")[0];
-// myEl.addEventListener("click", function(){
-//     console.log("Clicked the first <p>!");
-//   });
-//
-document.addEventListener('DOMContentLoaded', function () {
+// Need to add pause and stop buttons
 
-  const startButton = document.getElementById('start_button')
-  const sets = 2;
-  const reps = 3;
-  const repLength = 1;
-  const setRest = 5;
-  const exerciseName = 'curls';
+let counter = function(setsInput, repsInput, repLengthInput){
 
-  function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+  document.addEventListener('DOMContentLoaded', function () {
+    const sets = setsInput;
+    const reps = repsInput;
+    const minRepLength = 2;
+    const repLength = repLengthInput === 0 ? minRepLength : repLengthInput;
+    const setRest = 5;
 
-  var repsCounter = 1;
-  async function countReps() {
-    while (repsCounter <= reps) {
-      console.log(`Set ${setsCounter}, rep ${repsCounter}`);
-      await sleep(repLength * 1000);
-      repsCounter++
+    const startButton = document.getElementById('start_button');
+    const setDisplayer = document.getElementById('set');
+    const repDisplayer = document.getElementById('rep');
+    const repLengthDisplayer = document.getElementById('rep-length');
+    const beginFinishedIndicator = document.getElementById('begin-finished-indicator');
+
+    function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
     }
-    repsCounter = 1;
-    console.log(`Set ${setsCounter} complete`);
-  };
 
-  var setsCounter = 1;
-  async function countSets() {
-    while (setsCounter <= sets) {
-      console.log("Begin Set " + setsCounter);
-      await sleep(2 * 1000);
-      await countReps();
-      await sleep(setRest * 1000);
-      setsCounter++
-    }
-    setsCounter = 1;
-    console.log(`${exerciseName} complete`);
-  };
+    var repsCounter = 1;
+    async function countReps() {
+      while (repsCounter <= reps) {
+        console.log(`Set ${setsCounter}, rep ${repsCounter}`);
+        repDisplayer.textContent = repsCounter;
 
-  startButton.addEventListener('click', function (event) {
-    event.preventDefault;
-    console.log(event)
-    countSets();
+        await sleep(repLength * 1000);
+        repsCounter++
+      }
+      repsCounter = 1;
+      console.log(`Set ${setsCounter} complete`);
+      beginFinishedIndicator.textContent = 'Rest...';
+
+    };
+
+    var setsCounter = 1;
+    async function countSets() {
+      while (setsCounter <= sets) {
+        console.log("Begin Set " + setsCounter);
+        setDisplayer.textContent = setsCounter;
+        beginFinishedIndicator.textContent = 'Begin!';
+
+        await sleep(2 * 1000);
+        await countReps();
+        await sleep(setRest * 1000);
+        setsCounter++
+      }
+      setsCounter = 1;
+      console.log(`Exercise complete`)
+      beginFinishedIndicator.textContent = 'Finished!';
+    };
+
+    startButton.addEventListener('click', function (event) {
+      event.preventDefault;
+      beginFinishedIndicator.classList.add('hidden')
+
+      beginFinishedIndicator.textContent = 'Begin!';
+      beginFinishedIndicator.classList.remove('hidden')
+      countSets();
+    });
+
   });
-
-  
-});
+};
