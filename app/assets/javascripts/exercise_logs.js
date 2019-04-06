317755a37a55b23474304dfd81673b1d158fcbf5
Anne Richardson <richardson.ae@gmail.com>
2019-04-06 17:14:16 -0500

    Add tones to rep counter

--- app/assets/javascripts/exercise_logs.js
+++ app/assets/javascripts/exercise_logs.js
@@ -14,17 +14,23 @@ let counter = function(setsInput, repsInput, repLengthInput){
     const repDisplayer = document.getElementById('rep');
     const repLengthDisplayer = document.getElementById('rep-length');
     const beginFinishedIndicator = document.getElementById('begin-finished-indicator');
-
     const soundExerciseBegin = new Audio('/sounds/exercise_begin.wav');
-    var playButton = document.getElementById('play_button');
-
-    playButton.addEventListener('click', function (event) {
-      event.preventDefault();
-      soundExerciseBegin.play();
-    });
+    const soundExerciseComplete = new Audio('/sounds/exercise_complete.m4a');
+    const soundSetComplete = new Audio('/sounds/set_complete.m4a');
+    const soundRep = new Audio('/sounds/rep.m4a');
 
     function sleep(ms) {
       return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    function completeExercise() {
+      console.log(`Exercise complete`)
+      beginFinishedIndicator.textContent = 'Finished!';
+      setDisplayer.textContent = 0;
+      repDisplayer.textContent = 0;
+      soundExerciseComplete.play().catch(e => {
+        console.log(e);
+      });
     }
 
     var repsCounter = 1;
@@ -32,6 +38,9 @@ let counter = function(setsInput, repsInput, repLengthInput){
       while (repsCounter <= reps) {
         console.log(`Set ${setsCounter}, rep ${repsCounter}`);
         repDisplayer.textContent = repsCounter;
+        soundRep.play().catch(e => {
+          console.log(e);
+        });;
 
         await sleep(repLength * 1000);
         repsCounter++
@@ -39,11 +48,15 @@ let counter = function(setsInput, repsInput, repLengthInput){
       repsCounter = 1;
       console.log(`Set ${setsCounter} complete`);
       beginFinishedIndicator.textContent = 'Rest...';
-
+      repDisplayer.textContent = 0;
     };
 
     var setsCounter = 1;
     async function countSets() {
+      soundExerciseBegin.play().catch(e => {
+        console.log(e);
+      });;
+
       while (setsCounter <= sets) {
         console.log("Begin Set " + setsCounter);
         setDisplayer.textContent = setsCounter;
@@ -51,12 +64,14 @@ let counter = function(setsInput, repsInput, repLengthInput){
 
         await sleep(2 * 1000);
         await countReps();
+        soundSetComplete.play().catch(e => {
+          console.log(e);
+        });;
         await sleep(setRest * 1000);
         setsCounter++
       }
       setsCounter = 1;
-      console.log(`Exercise complete`)
-      beginFinishedIndicator.textContent = 'Finished!';
+      completeExercise();
     };
 
     startButton.addEventListener('click', function (event) {
