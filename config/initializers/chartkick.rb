dd6dbd259b7aa6bd96e994dc6932482b1a869d58
Anne Richardson <richardson.ae@gmail.com>
2019-04-11 21:02:26 -0500

    WIP remove custom colors

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -2,8 +2,8 @@ pain_color = '#F2B134'
 exercise_color = '#48AA6C'
 therapy_color = '#0994A1'
 
-Chartkick.options = {
-  height: "400px",
-  colors: [exercise_color, therapy_color, pain_color, ]
-  # colors: ['#F2B134', '#48AA6C', '#0994A1']
-}
+# Chartkick.options = {
+#   height: "400px",
+#   colors: [exercise_color, therapy_color, pain_color, ]
+#   # colors: ['#F2B134', '#48AA6C', '#0994A1']
+# }
