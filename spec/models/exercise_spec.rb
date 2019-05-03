136b0e86903b5b4a7a65e2e87bfef541c40fe84c
Anne Richardson <richardson.ae@gmail.com>
2019-05-03 17:07:19 -0500

    Add tests

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -49,4 +49,31 @@ RSpec.describe Exercise, type: :model do
       expect(Exercise.log_count_by_name).not_to include(insufficient_exercise)
     end
   end
+
+  describe 'self.search()' do
+    let!(:ex1) { create(:exercise, name: 'ex1') }
+    let!(:ex2) { create(:exercise, name: 'ex2') }
+
+    it 'returns all exercises if no argument is supplied' do
+      terms = ''
+      expect(Exercise.search(terms).count).to eq(2)
+    end
+
+    it 'returns all matches for partial string matches' do
+      terms = 'ex'
+      expect(Exercise.search(terms).count).to eq(2)
+    end
+
+    it 'excludes items that have character that are not included' do
+      terms = 'x1'
+      expect(Exercise.search(terms).count).to eq(1)
+      expect(Exercise.search(terms)).to include(ex1)
+      expect(Exercise.search(terms)).to_not include(ex2)
+    end
+
+    it 'returns an empty array if there are no matches' do
+      terms = 'ex3'
+      expect(Exercise.search(terms)).to eq([])
+    end
+  end
 end
