bf1e3cd19a8c1381c80afd4fce79f43f6ef47055
Anne Richardson <richardson.ae@gmail.com>
2019-05-14 21:29:52 -0500

    Delete klugey mystery table

--- db/migrate/20190515015221_drop_old_tables.rb
+++ db/migrate/20190515015221_drop_old_tables.rb
@@ -1,18 +1,10 @@
 class DropOldTables < ActiveRecord::Migration[5.2]
   def up
-    drop_table :homework_exercises
     drop_table :pt_session_exercises
   end
 
   def down
     create_table :pt_session_exercises do |t|
-      t.references :pt_session, foreign_key: true
-      t.references :exercise, foreign_key: true
-
-      t.timestamps
-    end
-
-    create_table :homework_exercises do |t|
       t.references :pt_session, foreign_key: true
       t.references :exercise, foreign_key: true
 
