9494355ae826aa79707dc18be79e41334b86647d
hiendinhngoc <hiendinhngoc@yahoo.com.vn>
2019-05-18 01:52:23 +0700

    #7 Get  models test pass

--- spec/models/exercise_log_spec.rb
+++ spec/models/exercise_log_spec.rb
@@ -5,6 +5,9 @@ require 'rails_helper'
 RSpec.describe ExerciseLog, type: :model do
   context 'associations' do
     it { should belong_to(:user) }
+    it { should belong_to(:exercise) }
+    it { should belong_to(:body_part) }
+    it { should belong_to(:pt_session).optional }
   end
 
   context 'validations' do
@@ -27,6 +30,78 @@ RSpec.describe ExerciseLog, type: :model do
   context 'delegations' do
     it { should delegate_method(:name).to(:body_part).with_prefix }
     it { should delegate_method(:name).to(:exercise).with_prefix }
+  end
+
+  describe 'self.at_home' do
+    it 'returns exercise_log if it doesn\'t belong to pt_session' do
+      exercise_log = create(:exercise_log, pt_session_id: nil)
+
+      expect(ExerciseLog.at_home.first).to eq exercise_log
+    end
+
+    it 'returns empty if exercise_logs belong to pt_sessions' do
+      user = create(:user)
+      pt_session = create(:pt_session, user_id: user.id)
+      exercise_log = build(:exercise_log, pt_session_id: pt_session.id)
+
+      expect(ExerciseLog.at_home).to be_empty
+    end
+  end
+
+  describe 'self.at_pt' do
+    it 'returns exercise_log if it belongs to pt_session' do
+      user = create(:user)
+      pt_session = create(:pt_session, user_id: user.id)
+      exercise_log = create(:exercise_log, pt_session_id: pt_session.id)
+
+      expect(ExerciseLog.at_pt.first).to eq exercise_log
+    end
+
+    it 'returns empty if exercise_log it doesn\'t belong to pt_session' do
+      exercise_log = create(:exercise_log, pt_session_id: nil)
+
+      expect(ExerciseLog.at_pt).to be_empty
+    end
+  end
+
+  describe 'self.past_week' do
+    it 'returns only the logs between today and the past 7 days' do
+      exercise_log = create(:exercise_log, datetime_occurred: Date.today.to_datetime - 2.days)
+
+      expect(ExerciseLog.past_week.first).to eq exercise_log
+    end
+
+    it 'returns empty if the datetime_occurreds are out of the range 7 days past' do
+      exercise_log1 = create(:exercise_log, datetime_occurred: Date.today.to_datetime - 8.days)
+      exercise_log2 = create(:exercise_log, datetime_occurred: Date.today.to_datetime + 2.days)
+
+      expect(ExerciseLog.past_week).to be_empty
+    end
+  end
+
+  describe 'self.past_two_weeks' do
+    it 'returns only the logs between today and the past 14 days' do
+      exercise_log = create(:exercise_log, datetime_occurred: Date.today.to_datetime - 12.days)
+
+      expect(ExerciseLog.past_two_weeks.first).to eq exercise_log
+    end
+
+    it 'returns empty if the datetime_occurreds are out of the range 14 days past' do
+      exercise_log1 = create(:exercise_log, datetime_occurred: Date.today.to_datetime - 20.days)
+      exercise_log2 = create(:exercise_log, datetime_occurred: Date.today.to_datetime + 2.days)
+
+      expect(ExerciseLog.past_two_weeks).to be_empty
+    end
+  end
+
+  describe 'self.group_by_exercise_and_count' do
+    let!(:exercise1) { create(:exercise, :with_3_exercise_logs, name: 'cross-body isometrics') }
+    let!(:exercise2) { create(:exercise, :with_3_exercise_logs, name: 'clam shells') }
+
+    it 'returns the exercise name and the count of its logs as a nested array' do
+      expected_output = [['cross-body isometrics', 3],['clam shells', 3]]
+      expect(ExerciseLog.group_by_exercise_and_count).to match_array(expected_output)
+    end
   end
 
   describe 'self.minutes_spent_by_day' do
@@ -77,6 +152,32 @@ RSpec.describe ExerciseLog, type: :model do
       allow(exercise_log).to receive(:seconds_spent).and_return(30)
 
       expect(exercise_log.minutes_spent).to eq(0.5)
+    end
+  end
+
+  describe '#blank_stats?' do
+    it 'returns false if both sets and reps are not blank' do
+      exercise_log = build(:exercise_log)
+      allow(exercise_log).to receive(:sets).and_return(1)
+      allow(exercise_log).to receive(:reps).and_return(2)
+
+      expect(exercise_log.blank_stats?).to eq false
+    end
+
+    it 'returns true if sets is blank and reps is not blank' do
+      exercise_log = build(:exercise_log)
+      allow(exercise_log).to receive(:sets).and_return('')
+      allow(exercise_log).to receive(:reps).and_return(2)
+
+      expect(exercise_log.blank_stats?).to eq true
+    end
+
+    it 'returns true if sets is not blank and reps is blank' do
+      exercise_log = build(:exercise_log)
+      allow(exercise_log).to receive(:sets).and_return(1)
+      allow(exercise_log).to receive(:reps).and_return('')
+
+      expect(exercise_log.blank_stats?).to eq true
     end
   end
 end
