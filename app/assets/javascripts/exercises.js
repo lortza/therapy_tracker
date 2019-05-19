49988975ba2007d8bd1b239d631832068e2b85a6
Anne Richardson <richardson.ae@gmail.com>
2019-05-19 18:20:42 -0500

    Restrict event listener to only this page

--- app/assets/javascripts/exercises.js
+++ app/assets/javascripts/exercises.js
@@ -1,22 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let exerciseList = document.getElementById('exercise-list')
+function listToggler() {
+  document.addEventListener('DOMContentLoaded', function () {
+    let exerciseList = document.getElementById('exercise-list')
 
-  exerciseList.addEventListener('click', function (event) {
-    let element = event.target
-    if(element.classList.contains('exercise-description-toggle')){
-      let description = element.parentElement.parentElement.querySelector('.exercise-description');
-      if(element.classList.contains('fa-chevron-down')){
-        element.classList.remove('fa-chevron-down')
-        element.classList.add('fa-chevron-left')
-        description.style.display = 'block';
-      } else {
-        element.classList.remove('fa-chevron-left')
-        element.classList.add('fa-chevron-down')
-        description.style.display = 'none';
+    exerciseList.addEventListener('click', function (event) {
+      let element = event.target
+      if(element.classList.contains('exercise-description-toggle')){
+        let description = element.parentElement.parentElement.querySelector('.exercise-description');
+        if(element.classList.contains('fa-chevron-down')){
+          element.classList.remove('fa-chevron-down')
+          element.classList.add('fa-chevron-left')
+          description.style.display = 'block';
+        } else {
+          element.classList.remove('fa-chevron-left')
+          element.classList.add('fa-chevron-down')
+          description.style.display = 'none';
+        }
       }
-    }
+    });
   });
-});
+}
 
 
 function dropdownSwitcher() {
