b34a57e6ab724f5043a62d1125cc87eef6ebfa97
Anne Richardson <richardson.ae@gmail.com>
2019-05-20 09:49:55 -0500

    Add button to log another ex_log from ex_log show page

--- app/helpers/exercise_logs_helper.rb
+++ app/helpers/exercise_logs_helper.rb
@@ -15,4 +15,12 @@ module ExerciseLogsHelper
   def destination_url(pt_session)
     pt_session ? pt_session_exercise_log_path(pt_session, @exercise_log) : exercise_log_path(@exercise_log)
   end
+
+  def new_exercise_log_button
+    if @pt_session
+      link_to '<i class="fal fa-hospital-user"></i> Log Another PT Exercise'.html_safe, new_pt_session_exercise_log_path(@pt_session), class: 'btn btn-outline btn-wide btn-info'
+    else
+      link_to '<i class="fal fa-dumbbell"></i> Log Another Exercise'.html_safe, new_exercise_log_path, class: 'btn btn-outline btn-wide btn-success'
+    end
+  end
 end
