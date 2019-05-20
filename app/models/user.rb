6c340522064b64eefcdf4945cddbe34b8f185e09
Anne Richardson <richardson.ae@gmail.com>
2019-05-20 18:22:52 -0500

    Add have_many expectations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :body_parts, dependent: :destroy
   has_many :exercises, dependent: :destroy
   has_many :exercise_logs, dependent: :destroy
+  has_many :pains, dependent: :destroy
   has_many :pain_logs, dependent: :destroy
   has_many :pt_sessions, dependent: :destroy
-  has_many :pains, dependent: :destroy
-  has_many :body_parts, dependent: :destroy
 
   def full_name
     "#{first_name} #{last_name}"
