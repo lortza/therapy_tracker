a4448afa0f822f63e081c152f286938d98ea6db4
Anne Richardson <richardson.ae@gmail.com>
2019-05-23 10:59:23 -0500

    Makes name validation case-sensitive

--- spec/models/pain_spec.rb
+++ spec/models/pain_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Pain, type: :model do
 
   context 'validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_uniqueness_of(:name) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
   end
 
   describe 'self.has_logs' do
