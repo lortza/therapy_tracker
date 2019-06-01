76bbf3fa014d856730177dcd1608922b08215902
richardsonae <richardson.ae@gmail.com>
2019-06-01 12:53:00 -0500

    55 landing page content (#70)
    * Scope name-uniqueness to user
    
    * Show button to add new if no records
    
    * Add form-control
    
    * Add examples of names
    
    * Add html structure for new users' landing page
    
    * Move new-user content into partial
    
    * Conditionally display sections

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Exercise, type: :model do
   end
 
   context 'validations' do
-    it { should validate_uniqueness_of(:name) }
+    it { should validate_uniqueness_of(:name).case_insensitive.scoped_to(:user_id) }
 
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
