7211bb873c5eb38eaf0218bab0d7e67c2fa7a27e
Anne Richardson <richardson.ae@gmail.com>
2019-06-02 18:54:20 -0500

    Get view specs passing

--- spec/views/body_parts/edit.html.erb_spec.rb
+++ spec/views/body_parts/edit.html.erb_spec.rb
@@ -4,8 +4,11 @@ require 'rails_helper'
 
 RSpec.describe 'body_parts/edit', type: :view do
   before(:each) do
+    @user = create(:user)
     @body_part = assign(:body_part, BodyPart.create!(
-                                      name: 'MyString'
+                                      user_id: @user.id,
+                                      name: 'MyString',
+                                      archived: false
                                     ))
   end
 
@@ -14,6 +17,7 @@ RSpec.describe 'body_parts/edit', type: :view do
 
     assert_select 'form[action=?][method=?]', body_part_path(@body_part), 'post' do
       assert_select 'input[name=?]', 'body_part[name]'
+      assert_select 'input[name=?]', 'body_part[archived]'
     end
   end
 end
--- spec/views/body_parts/new.html.erb_spec.rb
+++ spec/views/body_parts/new.html.erb_spec.rb
@@ -4,9 +4,12 @@ require 'rails_helper'
 
 RSpec.describe 'body_parts/new', type: :view do
   before(:each) do
+    @user = create(:user)
     assign(:body_part, BodyPart.new(
-                         name: 'MyString'
-                       ))
+                                user_id: @user.id,
+                                name: 'MyString',
+                                archived: false
+                              ))
   end
 
   it 'renders new body_part form' do
@@ -14,6 +17,7 @@ RSpec.describe 'body_parts/new', type: :view do
 
     assert_select 'form[action=?][method=?]', body_parts_path, 'post' do
       assert_select 'input[name=?]', 'body_part[name]'
+      assert_select 'input[name=?]', 'body_part[archived]'
     end
   end
 end
