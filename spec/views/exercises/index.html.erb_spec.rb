fd24815fc2dc055d2a1258264de1a5a7d7b280d9
Anne Richardson <richardson.ae@gmail.com>
2019-06-03 08:47:14 -0500

    Add view specs

--- spec/views/exercises/index.html.erb_spec.rb
+++ spec/views/exercises/index.html.erb_spec.rb
@@ -4,12 +4,16 @@ require 'rails_helper'
 
 RSpec.describe 'exercises/index', type: :view do
   before(:each) do
-    assign(:exercises, [create(:exercise, default_sets: 1, default_reps: 1, default_rep_length: 1),
-                        create(:exercise, default_sets: 1, default_reps: 1, default_rep_length: 1),])
+    @user = create(:user)
+    assign(:exercises, [create(:exercise, user_id: @user.id, default_sets: 1, default_reps: 1, default_rep_length: 1),
+                        create(:exercise, user_id: @user.id, default_sets: 1, default_reps: 1, default_rep_length: 1),])
   end
 
   it 'renders a list of exercises' do
+    allow(view).to receive(:current_user).and_return(@user)
+
     render
+    assert_select 'form'
     assert_select 'article>p>strong', text: '1 sets of 1 with 1 sec hold'.to_s, count: 2
   end
 end
