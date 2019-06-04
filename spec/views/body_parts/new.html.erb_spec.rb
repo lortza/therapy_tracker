f3042e4053c575d7c93254d47e7ebdc329a495b8
Anne Richardson <richardson.ae@gmail.com>
2019-06-03 19:31:30 -0500

    Appease Rubo :cop:

--- spec/views/body_parts/new.html.erb_spec.rb
+++ spec/views/body_parts/new.html.erb_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe 'body_parts/new', type: :view do
   before(:each) do
     @user = create(:user)
     assign(:body_part, BodyPart.new(
-                                user_id: @user.id,
-                                name: 'MyString',
-                                archived: false
-                              ))
+                         user_id: @user.id,
+                         name: 'MyString',
+                         archived: false
+                       ))
   end
 
   it 'renders new body_part form' do
