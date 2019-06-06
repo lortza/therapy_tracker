34e5f3c66d79e5504fc798d72441373e71fa4943
Kelsey Huse <kelseyhuse30@gmail.com>
2019-06-05 19:38:56 -0500

    exercise log specs

--- spec/views/exercise_logs/index.html.erb_spec.rb
+++ spec/views/exercise_logs/index.html.erb_spec.rb
@@ -4,37 +4,15 @@ require 'rails_helper'
 
 RSpec.describe 'exercise_logs/index', type: :view do
   before(:each) do
-    assign(:exercise_logs, [
-             ExerciseLog.create!(
-               sets: 2,
-               reps: 3,
-               datetime_occurred: 'Sun, 24 Mar 2019 09:30:00 UTC +00:00',
-               body_part_id: 1,
-               current_pain_level: 4,
-               current_pain_frequency: 'Current Pain Frequency',
-               progress_note: 'MyText'
-             ),
-             ExerciseLog.create!(
-               sets: 2,
-               reps: 3,
-               datetime_occurred: 'Sun, 24 Mar 2019 09:30:00 UTC +00:00',
-               body_part_id: 1,
-               current_pain_level: 4,
-               current_pain_frequency: 'Current Pain Frequency',
-               progress_note: 'MyText'
-             ),
-           ])
+    @user = create(:user)
+    create(:exercise_log, user_id: @user.id)
+    create(:exercise_log, user_id: @user.id)
+    @logs = @user.exercise_logs.at_home
+                 .paginate(page: params[:page], per_page: 25)
   end
 
   it 'renders a list of exercise_logs' do
     render
-    assert_select 'tr>td', text: 2.to_s, count: 2
-    assert_select 'tr>td', text: 3.to_s, count: 2
-    assert_select 'tr>td', text: 'Sun, 24 Mar 2019 09:30:00 UTC +00:00'.to_s, count: 2
-    assert_select 'tr>td', text: 'Body Part Name'.to_s, count: 2
-    assert_select 'tr>td', text: 'Exercise Name'.to_s, count: 2
-    assert_select 'tr>td', text: 4.to_s, count: 2
-    assert_select 'tr>td', text: 'Current Pain Frequency'.to_s, count: 2
-    assert_select 'tr>td', text: 'MyText'.to_s, count: 2
+    assert_select 'div.card-body>p', count: 2
   end
 end
