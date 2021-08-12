2f07cf707c4ca1cc768cb08e10b4ffb810158a6c
Anne Richardson <richardson.ae@gmail.com>
2021-08-12 10:42:29 -0500

    Update request specs

--- spec/routing/charts_routing_spec.rb
+++ spec/routing/charts_routing_spec.rb
@@ -2,10 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.describe ReportsController, type: :routing do
+RSpec.describe ChartsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/reports').to route_to('reports#index')
+      expect(get: '/charts').to route_to('charts#index')
     end
   end
 end
