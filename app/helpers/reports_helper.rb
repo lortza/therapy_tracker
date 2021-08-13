f4d1103516411efeb77f87f2ba26caa31fd1f4dc
Anne Richardson <richardson.ae@gmail.com>
2021-08-12 20:34:31 -0500

    Fix activerecord collision with occurrence ordering

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -4,4 +4,12 @@ module ReportsHelper
   def avg_pain_level(occurrences)
     (occurrences.map(&:pain_level).sum / occurrences.length).to_i
   end
+
+  def first_occurrence(occurrences)
+    occurrences.map(&:datetime_occurred).min
+  end
+
+  def last_occurrence(occurrences)
+    occurrences.map(&:datetime_occurred).max
+  end
 end
--- spec/helpers/reports_helper_spec.rb
+++ spec/helpers/reports_helper_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe ReportsHelper, type: :helper do
+  let(:user) { create(:user) }
+
   describe 'avg_pain_level' do
     let(:pain) { create(:pain) }
 
@@ -18,6 +20,53 @@ RSpec.describe ReportsHelper, type: :helper do
       create(:pain_log, pain: pain, pain_level: 2)
 
       expect(helper.avg_pain_level(pain.logs)).to eq(2)
+    end
+  end
+
+  context 'occurrences ordering' do
+    let(:ache) { create(:pain, user: user) }
+    let(:neck) { create(:body_part, user: user) }
+    let!(:pain_log_today) do
+      create(:pain_log, user: user, pain: ache, body_part: neck, datetime_occurred: 5.hours.ago)
+    end
+    let!(:pain_log_yesterday) do
+      create(:pain_log, user: user, pain: ache, body_part: neck, datetime_occurred: 24.hours.ago)
+    end
+
+    let(:arse) { create(:body_part) }
+    let!(:arse_pain_log_today) do
+      create(:pain_log, user: user, pain: ache, body_part: arse, datetime_occurred: 2.hours.ago)
+    end
+    let!(:arse_pain_log_yesterday) do
+      create(:pain_log, user: user, pain: ache, body_part: arse, datetime_occurred: 25.hours.ago)
+    end
+
+    let(:neck_report) { Report.new(user: user,timeframe: '', body_part_id: neck.id) }
+
+    let(:neck_occurrences) do
+      neck_report.pain_stats_by_body_part.map { |pain, occurrences| occurrences }.first
+    end
+
+    describe 'first_occurrence' do
+      it 'returns the log with the oldest datetime_occurred' do
+        expect(helper.first_occurrence(neck_occurrences)).to eq(pain_log_yesterday.datetime_occurred)
+        expect(helper.first_occurrence(neck_occurrences)).to_not eq(pain_log_today.datetime_occurred)
+      end
+
+      it 'does not include records outside of the body_part set' do
+        expect(helper.first_occurrence(neck_occurrences)).to_not eq(arse_pain_log_yesterday.datetime_occurred)
+      end
+    end
+
+    describe 'last_occurrence' do
+      it 'returns the log with the most recent datetime_occurred' do
+        expect(helper.last_occurrence(neck_occurrences)).to eq(pain_log_today.datetime_occurred)
+        expect(helper.last_occurrence(neck_occurrences)).to_not eq(pain_log_yesterday.datetime_occurred)
+      end
+
+      it 'does not include records outside of the body_part set' do
+        expect(helper.last_occurrence(neck_occurrences)).to_not eq(arse_pain_log_today.datetime_occurred)
+      end
     end
   end
 end
