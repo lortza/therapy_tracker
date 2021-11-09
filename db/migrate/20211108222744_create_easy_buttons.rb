9158b954ce8ebb80e1d6169b8b517da2ee6211e0
Anne Richardson <richardson.ae@gmail.com>
2021-11-08 19:20:36 -0600

    Add easy_buttons table

---
+++ db/migrate/20211108222744_create_easy_buttons.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreateEasyButtons < ActiveRecord::Migration[6.1]
+  def change
+    create_table :easy_buttons do |t|
+      t.references :user, foreign_key: true
+      t.references :body_part, foreign_key: true
+      t.references :pain, foreign_key: true
+      t.string :name, null: false
+      t.integer :pain_level
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_030411) do
+ActiveRecord::Schema.define(version: 2021_11_08_222744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,19 @@ ActiveRecord::Schema.define(version: 2019_12_06_030411) do
     t.bigint "user_id"
     t.boolean "archived", default: false
     t.index ["user_id"], name: "index_body_parts_on_user_id"
+  end
+
+  create_table "easy_buttons", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "body_part_id"
+    t.bigint "pain_id"
+    t.string "name", null: false
+    t.integer "pain_level"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["body_part_id"], name: "index_easy_buttons_on_body_part_id"
+    t.index ["pain_id"], name: "index_easy_buttons_on_pain_id"
+    t.index ["user_id"], name: "index_easy_buttons_on_user_id"
   end
 
   create_table "exercise_logs", force: :cascade do |t|
@@ -152,6 +165,9 @@ ActiveRecord::Schema.define(version: 2019_12_06_030411) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "body_parts", "users"
+  add_foreign_key "easy_buttons", "body_parts"
+  add_foreign_key "easy_buttons", "pains"
+  add_foreign_key "easy_buttons", "users"
   add_foreign_key "exercise_logs", "body_parts"
   add_foreign_key "exercise_logs", "exercises"
   add_foreign_key "exercise_logs", "pt_session_logs"
