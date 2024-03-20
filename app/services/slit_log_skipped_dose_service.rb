9fe748fe78201a67e538f39caaa6d542a10bf068
Anne Richardson <richardson.ae@gmail.com>
2024-03-20 13:01:40 -0400

    omg remove pry

--- app/services/slit_log_skipped_dose_service.rb
+++ app/services/slit_log_skipped_dose_service.rb
@@ -10,7 +10,6 @@ class SlitLogSkippedDoseService
         Rails.logger.debug "last_log: #{last_log.occurred_at}"
         Rails.logger.debug "last_log.occurred_at.to_date: #{last_log.occurred_at.to_date}"
         Rails.logger.debug "Date.yesterday: #{Date.yesterday}"
-        binding.pry
         Rails.logger.debug "no_log_yesterday?: #{no_log_yesterday?(last_log)}"
         Rails.logger.debug "yes log yesterday: #{last_log&.occurred_at&.to_date == Date.yesterday}"
         next unless no_log_yesterday?(last_log)
