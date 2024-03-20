5d82296e9fb7421de8540af0fea358ed987a3844
Anne Richardson <richardson.ae@gmail.com>
2024-03-20 12:52:07 -0400

    Add logs to service

--- app/services/slit_log_skipped_dose_service.rb
+++ app/services/slit_log_skipped_dose_service.rb
@@ -1,12 +1,29 @@
 # frozen_string_literal: true
 
 class SlitLogSkippedDoseService
-  def self.call
-    User.with_slit_enabled.each do |user|
-      last_log = user.slit_logs.order(occurred_at: :desc).limit(1).first
-      next unless last_log.nil? || (last_log.occurred_at.to_date != Date.yesterday)
+  class << self
+    def call
+      User.with_slit_enabled.each do |user|
+        Rails.logger.debug "==============SlitLogSkippedDoseService=================="
+        Rails.logger.debug "user: #{user.email}"
+        last_log = user.slit_logs.order(occurred_at: :desc).limit(1).first
+        Rails.logger.debug "last_log: #{last_log.occurred_at}"
+        Rails.logger.debug "last_log.occurred_at.to_date: #{last_log.occurred_at.to_date}"
+        Rails.logger.debug "Date.yesterday: #{Date.yesterday}"
+        Rails.logger.debug "no_log_yesterday?: #{no_log_yesterday?(last_log)}"
+        Rails.logger.debug "yes log yesterday: #{last_log&.occurred_at&.to_date == Date.yesterday}"
+        next unless no_log_yesterday?(last_log)
+        Rails.logger.debug "DateTime.yesterday: #{DateTime.yesterday}"
+        Rails.logger.debug "Creating SLIT log for: #{DateTime.yesterday}"
+        user.slit_logs.create(occurred_at: DateTime.yesterday, dose_skipped: true)
+        Rails.logger.debug "========================================================="
+      end
 
-      user.slit_logs.create(occurred_at: DateTime.yesterday, dose_skipped: true)
+      private
+
+      def no_log_yesterday?(last_log)
+        last_log.nil? || (last_log.occurred_at.to_date != Date.yesterday)
+      end
     end
   end
 end
