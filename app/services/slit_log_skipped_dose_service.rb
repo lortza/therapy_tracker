8a70e0dce5dc52482c0fba97be1d7d39d6aa1427
Anne Richardson <richardson.ae@gmail.com>
2024-03-20 12:59:06 -0400

    Move private method

--- app/services/slit_log_skipped_dose_service.rb
+++ app/services/slit_log_skipped_dose_service.rb
@@ -10,6 +10,7 @@ class SlitLogSkippedDoseService
         Rails.logger.debug "last_log: #{last_log.occurred_at}"
         Rails.logger.debug "last_log.occurred_at.to_date: #{last_log.occurred_at.to_date}"
         Rails.logger.debug "Date.yesterday: #{Date.yesterday}"
+        binding.pry
         Rails.logger.debug "no_log_yesterday?: #{no_log_yesterday?(last_log)}"
         Rails.logger.debug "yes log yesterday: #{last_log&.occurred_at&.to_date == Date.yesterday}"
         next unless no_log_yesterday?(last_log)
@@ -18,12 +19,12 @@ class SlitLogSkippedDoseService
         user.slit_logs.create(occurred_at: DateTime.yesterday, dose_skipped: true)
         Rails.logger.debug "========================================================="
       end
+    end
 
-      private
+    private
 
-      def no_log_yesterday?(last_log)
-        last_log.nil? || (last_log.occurred_at.to_date != Date.yesterday)
-      end
+    def no_log_yesterday?(last_log)
+      last_log.nil? || (last_log.occurred_at.to_date != Date.yesterday)
     end
   end
 end
