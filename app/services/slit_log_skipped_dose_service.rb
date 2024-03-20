42cafde62c92847bca9aca47fb60abfdfda644f5
Anne Richardson <richardson.ae@gmail.com>
2024-03-20 13:15:40 -0400

    Output via puts instead

--- app/services/slit_log_skipped_dose_service.rb
+++ app/services/slit_log_skipped_dose_service.rb
@@ -4,20 +4,19 @@ class SlitLogSkippedDoseService
   class << self
     def call
       User.with_slit_enabled.each do |user|
-        Rails.logger.debug "==============SlitLogSkippedDoseService=================="
-        puts "putsing just for fun"
-        Rails.logger.debug "user: #{user.email}"
+        puts "==============SlitLogSkippedDoseService=================="
+        puts "user: #{user.email}"
         last_log = user.slit_logs.order(occurred_at: :desc).limit(1).first
-        Rails.logger.debug "last_log: #{last_log.occurred_at}"
-        Rails.logger.debug "last_log.occurred_at.to_date: #{last_log.occurred_at.to_date}"
-        Rails.logger.debug "Date.yesterday: #{Date.yesterday}"
-        Rails.logger.debug "no_log_yesterday?: #{no_log_yesterday?(last_log)}"
-        Rails.logger.debug "yes log yesterday: #{last_log&.occurred_at&.to_date == Date.yesterday}"
+        puts "last_log: #{last_log.occurred_at}"
+        puts "last_log.occurred_at.to_date: #{last_log.occurred_at.to_date}"
+        puts "Date.yesterday: #{Date.yesterday}"
+        puts "no_log_yesterday?: #{no_log_yesterday?(last_log)}"
+        puts "yes log yesterday: #{last_log&.occurred_at&.to_date == Date.yesterday}"
         next unless no_log_yesterday?(last_log)
-        Rails.logger.debug "DateTime.yesterday: #{DateTime.yesterday}"
-        Rails.logger.debug "Creating SLIT log for: #{DateTime.yesterday}"
+        puts "DateTime.yesterday: #{DateTime.yesterday}"
+        puts "Creating SLIT log for: #{DateTime.yesterday}"
         user.slit_logs.create(occurred_at: DateTime.yesterday, dose_skipped: true)
-        Rails.logger.debug "========================================================="
+        puts "========================================================="
       end
     end
 
