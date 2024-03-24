810fe9b944ca4ec9d7631268eee9b7f018719fc8
richardsonae <richardson.ae@gmail.com>
2024-03-24 14:41:35 -0400

    Allow user profile editing (#782)
    * Add ability for user to edit preferences
    
    * Add request specs
    
    * Conditinally render new SLIT log links
    
    * Rubocop

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update]
+  before_action :set_return_to_destination, only: [:edit]
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to session.fetch(:return_to, root_path)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_return_to_destination
+    session[:return_to] ||= request.referer
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:enable_slit_tracking)
+  end
+end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  let!(:user) { create(:user) }
+
+  describe 'Public access to users' do
+    it 'denies access to users#edit' do
+      get edit_user_path(user.id)
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it 'denies access to users#update' do
+      patch user_path(user, user: user.attributes)
+      expect(response).to redirect_to new_user_session_path
+    end
+  end
+
+  describe 'Authenticated access to users' do
+    before { sign_in(user) }
+
+    describe '#edit' do
+      it 'renders users#edit' do
+        get edit_user_path(user.id)
+
+        expect(response).to be_successful
+        expect(response).to render_template(:edit)
+      end
+    end
+
+    describe '#update' do
+      let(:user) { create(:user, enable_slit_tracking: false) }
+
+      it 'updates the user' do
+        patch user_path(user, user: { id: user.id, enable_slit_tracking: true })
+
+        expect(user.reload.enable_slit_tracking).to eq(true)
+      end
+
+      context 'when there is a return_to location' do
+        it 'redirects back to the previous url' do
+          session = { return_to: exercise_logs_path }
+          allow_any_instance_of(UsersController).to receive(:session).and_return(session)
+          patch user_path(user, user: { id: user.id, enable_slit_tracking: true })
+
+          expect(response).to redirect_to(exercise_logs_path)
+        end
+      end
+
+      context 'when there is not a return_to location' do
+        it 'redirects to the root_url' do
+          patch user_path(user, user: { id: user.id, enable_slit_tracking: true })
+
+          expect(response).to redirect_to(root_url)
+        end
+      end
+    end
+  end
+end
