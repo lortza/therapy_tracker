1443173e2f93e9379449f1e495a01ccf60201d90
Anne Richardson <richardson.ae@gmail.com>
2024-03-31 15:35:28 -0400

    Derive path automatically

--- app/components/new_log_button_component.rb
+++ app/components/new_log_button_component.rb
@@ -1,30 +1,36 @@
 # frozen_string_literal: true
 
-# link_to MaterialIconComponent.new(icon: ExerciseLog.new.decorate.icon_name, size: :large).render + '<br>+Exercise'.html_safe, new_exercise_log_path, class: 'add-log-button exercise-button'
-
 class NewLogButtonComponent
-  # include ActionView::Helpers::TagHelper
-
-  def initialize(log_klass:, text: nil, path:, method: :get, classes: nil)
+  def initialize(log_klass:, text: nil, path: nil, method: :get)
     @log_klass = log_klass
-    @text = text || decorated_log.display_name
+    @text = text
     @path = path
     @method = method
-    @classes = classes
   end
 
   def render
-    ActionController::Base.helpers.link_to(button_text, @path, method: @method, class: computed_classes)
+    ActionController::Base.helpers.link_to(button_text, computed_path, method: @method, class: computed_classes)
   end
 
   private
 
   def button_text
-    MaterialIconComponent.new(icon: decorated_log.icon_name, size: :large).render + "<br>+#{@text}".html_safe
+    MaterialIconComponent.new(
+      icon: decorated_log.icon_name,
+      size: :large
+    ).render + "<br>+#{computed_text}".html_safe
   end
 
   def decorated_log
     @decorated_log ||= @log_klass.new.decorate
+  end
+
+  def computed_path
+    @path || "#{@log_klass.to_s.underscore}s/new"
+  end
+
+  def computed_text
+    @text || decorated_log.display_name
   end
 
   def computed_classes
