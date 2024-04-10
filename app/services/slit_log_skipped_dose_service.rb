123fd958a9da23eee5076b200a98146db8711f6a
Anne Richardson <richardson.ae@gmail.com>
2024-04-09 21:36:59 -0400

    Add data jsonb to dummy_records

--- app/services/slit_log_skipped_dose_service.rb
+++ app/services/slit_log_skipped_dose_service.rb
@@ -6,6 +6,16 @@ class SlitLogSkippedDoseService
       yesterday = Date.yesterday
 
       User.with_slit_enabled.each do |user|
+        DummyRecord.create(data:
+          {
+            user: user.email,
+            service: "SlitLogSkippedDoseService",
+            no_log_for_user_yesterday?: no_log_for_user_yesterday?(user: user, yesterday: yesterday),
+            yesterday: yesterday,
+            yesterday_beginning_of_day: yesterday.beginning_of_day,
+            yesterday_end_of_day: yesterday.end_of_day
+          }
+        )
         next unless no_log_for_user_yesterday?(user: user, yesterday: yesterday)
 
         user.slit_logs.create(occurred_at: DateTime.yesterday, dose_skipped: true)
