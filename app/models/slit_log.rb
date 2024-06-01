6b799f92b23dd51a16b103d83bd31c5175ffb64e
Anne Richardson <richardson.ae@gmail.com>
2024-06-01 15:06:43 -0400

    Add dose countdown

--- app/models/slit_log.rb
+++ app/models/slit_log.rb
@@ -4,4 +4,36 @@ class SlitLog < ApplicationRecord
   belongs_to :user
 
   validates :occurred_at, presence: true
+
+  before_save :set_doses_remaining
+
+  MAX_BOTTLE_DOSES = 45
+
+  private
+
+  def set_doses_remaining
+    if started_new_bottle?
+      self.doses_remaining = MAX_BOTTLE_DOSES
+    else
+      previous_balance = previous_log&.doses_remaining
+      return nil unless previous_balance.present?
+
+      self.doses_remaining = calculate_doses_remaining(previous_balance)
+    end
+  end
+
+  def previous_log
+    current_stamp = DateTime.current
+    SlitLog.where('occurred_at < ?', current_stamp).order(occurred_at: :desc).first
+  end
+
+  def calculate_doses_remaining(previous_log_doses_remaining)
+    if dose_skipped?
+      previous_log_doses_remaining
+    elsif previous_log_doses_remaining > 0
+      previous_log_doses_remaining - 1
+    else
+      0
+    end
+  end
 end
--- spec/models/slit_log_spec.rb
+++ spec/models/slit_log_spec.rb
@@ -10,4 +10,70 @@ RSpec.describe SlitLog, type: :model do
   context 'validations' do
     it { should validate_presence_of(:occurred_at) }
   end
+
+  describe 'before_save actions' do
+    describe 'private: set_doses_remaining' do
+      before do
+        user = create(:user)
+        create(:slit_log, user: user, occurred_at: DateTime.current - 2.days, doses_remaining: previous_balance)
+      end
+
+      context 'when starting a new bottle' do
+        let(:slit_log) { build(:slit_log, started_new_bottle: true) }
+        let(:previous_balance) { nil }
+
+        it 'sets the doses_remaining to the default value' do
+          slit_log.send(:set_doses_remaining)
+          expect(slit_log.doses_remaining).to eq(SlitLog::MAX_BOTTLE_DOSES)
+        end
+      end
+
+      context "when a previous log's doses_remaining is not available" do
+        let(:slit_log) { build(:slit_log) }
+        let(:previous_balance) { nil }
+
+        it 'sets the doses_remaining to nil' do
+          slit_log.send(:set_doses_remaining)
+          expect(slit_log.doses_remaining).to be(nil)
+        end
+      end
+
+      context "when a previous log's doses_remaining is available" do
+        let(:slit_log) { build(:slit_log) }
+        let(:previous_balance) { 1 }
+
+        it 'calls calculate_doses_remaining' do
+          expect(slit_log).to receive(:calculate_doses_remaining)
+          slit_log.send(:set_doses_remaining)
+        end
+      end
+    end
+  end
+
+  describe 'private: #calculate_doses_remaining' do
+    context 'when the current log is a skipped dose' do
+      it "uses the previous log's value" do
+        previous_value = 5
+        expect(SlitLog.new(dose_skipped: true).send(:calculate_doses_remaining, previous_value)).to eq(previous_value)
+      end
+    end
+
+    context "when the previous log's value is greater than zero" do
+      it "returns the previous log's value minus 1" do
+        expect(SlitLog.new.send(:calculate_doses_remaining, 5)).to eq(4)
+      end
+    end
+
+    context "when the previous log's value is zero" do
+      it "returns 0" do
+        expect(SlitLog.new.send(:calculate_doses_remaining, 0)).to eq(0)
+      end
+    end
+
+    context "when the previous log's value is less than zero" do
+      it "returns 0" do
+        expect(SlitLog.new.send(:calculate_doses_remaining, -1)).to eq(0)
+      end
+    end
+  end
 end
