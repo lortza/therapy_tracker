19fddd364c0207592ce598124858b767f3ceee72
Anne Richardson <richardson.ae@gmail.com>
2024-06-03 08:05:46 -0400

    Move log seeds into rake tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ User.destroy_all
 #-------------------------------------------
 #            CREATE USER
 #-------------------------------------------
-puts 'Building Users'
+puts 'Seeding Users'
 existing_user = User.find_by(email: 'admin@email.com', admin: true)
 user = existing_user.present? ? existing_user : FactoryBot.create(:user, first_name: 'Admin', last_name: 'McAdmins', email: 'admin@email.com', admin: true)
 
@@ -25,14 +25,14 @@ user = existing_user.present? ? existing_user : FactoryBot.create(:user, first_n
 #-------------------------------------------
 #            BUILD BASE CLASSES
 #-------------------------------------------
-puts 'Building Exercises'
+puts 'Seeding Exercises'
 FactoryBot.create(:exercise, user_id: user.id, name: 'clam shells', description: 'lie on one side with legs bent at knees', default_per_side: true )
 FactoryBot.create(:exercise, user_id: user.id, name: 'wall slides', description: 'lie on side with body flat against wall. raise leg up, sliding heel againt wall.', default_per_side: true )
 FactoryBot.create(:exercise, user_id: user.id, name: 'bridges', description: 'lie on back with legs bent at knees. lift butt up.' )
 FactoryBot.create(:exercise, user_id: user.id, name: 'cross-body isometrics', description: 'lie on back with legs bent at knees. bring right knee towards chest but push back with left hand. hold and engage core. alternate sides.' )
 
 #------------------------------------------
-puts 'Building Body Parts'
+puts 'Seeding Body Parts'
 body_part_names = ['hip, right', 'hip, left', 'knee, right', 'knee, left']
 
 body_part_names.each do |body_part_name|
@@ -41,7 +41,7 @@ end
 
 
 #------------------------------------------
-puts 'Building Pains'
+puts 'Seeding Pains'
 pain_names = ['aching', 'burning', 'throbbing', 'stabbing', 'stiffness']
 
 pain_names.each do |pain_name|
@@ -52,8 +52,11 @@ end
 #-------------------------------------------
 #              BUILD LOGS
 #-------------------------------------------
-puts 'Building Logs'
+puts 'Seeding Logs...'
 Rake::Task["db:seed:exercise_logs"].invoke(user.id, 30)
 Rake::Task["db:seed:pain_logs"].invoke(user.id, 30)
 Rake::Task["db:seed:pt_session_logs"].invoke(user.id, 30)
 Rake::Task["db:seed:slit_logs"].invoke(user.id, 90)
+
+
+puts "Done."
