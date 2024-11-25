b688c0056de3674e1658a3086e733001b39fae3d
Anne Richardson <richardson.ae@gmail.com>
2024-11-25 09:31:49 -0500

    Upgrade to Rails 7.2.2

--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,12 @@ Bundler.require(*Rails.groups)
 module TherapyTracker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -26,7 +26,5 @@ module TherapyTracker
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-
-    config.active_support.cache_format_version = 7.1
   end
 end
