2e3d546aad47ca7dbe0197966a9397e5fa40530c
Anne Richardson <richardson.ae@gmail.com>
2026-02-09 15:56:20 -0500

    Enable db ext pgcrypto for uuids

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_05_154650) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_09_205457) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "pgcrypto"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
