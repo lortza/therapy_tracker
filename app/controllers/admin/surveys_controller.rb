6f501bd92e9f1ccefcaf5d6a58c5e10e25e34449
Anne Richardson <richardson.ae@gmail.com>
2026-04-24 17:03:40 -0400

    Add routes for status-changing actions

--- app/controllers/admin/surveys_controller.rb
+++ app/controllers/admin/surveys_controller.rb
@@ -1,11 +1,48 @@
 class Admin::SurveysController < AdminController
-  before_action :set_survey, only: [:show]
+  before_action :set_survey, only: [:show, :publish, :archive, :make_public, :make_private, :destroy]
 
   def index
-    @surveys = Survey.all
+    @surveys = current_user.surveys.all
+    @public_surveys = Survey.where(available_to_public: true).where.not(user_id: current_user.id)
   end
 
   def show
+    authorize! @survey
+  end
+
+  def publish
+    authorize! @survey
+
+    @survey.published!
+    redirect_to admin_survey_path(@survey), notice: "Survey published successfully."
+  end
+
+  def archive
+    authorize! @survey
+
+    @survey.archived!
+    redirect_to admin_survey_path(@survey), notice: "Survey archived successfully."
+  end
+
+  def make_public
+    authorize! @survey
+
+    @survey.update(available_to_public: true)
+    redirect_to admin_survey_path(@survey), notice: "Survey is now public."
+  end
+
+  def make_private
+    authorize! @survey, :make_private?
+
+    @survey.update(available_to_public: false)
+    redirect_to admin_survey_path(@survey), notice: "Survey is now private."
+  end
+
+  def destroy
+    authorize! @survey, :destroy?
+
+    @survey.destroy
+    redirect_to admin_surveys_path, notice: "Survey deleted successfully."
   end
 
   private
