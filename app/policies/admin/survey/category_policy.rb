c691a1af41ad0d3d9d69eebf15ac7f20ec337250
Anne Richardson <richardson.ae@gmail.com>
2026-04-29 10:57:41 -0400

    Add category controller and policy

---
+++ app/policies/admin/survey/category_policy.rb
@@ -0,0 +1,21 @@
+class Admin::Survey::CategoryPolicy < ApplicationPolicy
+  def new?
+    allowed_to?(:edit?, record.survey, with: Admin::SurveyPolicy)
+  end
+
+  def create?
+    allowed_to?(:edit?, record.survey, with: Admin::SurveyPolicy)
+  end
+
+  def edit?
+    allowed_to?(:edit?, record.survey, with: Admin::SurveyPolicy)
+  end
+
+  def update?
+    allowed_to?(:edit?, record.survey, with: Admin::SurveyPolicy)
+  end
+
+  def destroy?
+    allowed_to?(:edit?, record.survey, with: Admin::SurveyPolicy)
+  end
+end
