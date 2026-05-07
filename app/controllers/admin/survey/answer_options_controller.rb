2a3d6c255566adb040ce82eeaa66fc8a62c004fe
Anne Richardson <richardson.ae@gmail.com>
2026-05-07 14:33:42 -0400

    Provide a predicted value for each new answer option
    This is just a convenience for the user since they will normally be adding values sequentially.

--- app/controllers/admin/survey/answer_options_controller.rb
+++ app/controllers/admin/survey/answer_options_controller.rb
@@ -3,7 +3,7 @@ class Admin::Survey::AnswerOptionsController < AdminController
   before_action :set_survey, only: [:new, :create, :edit, :update, :destroy]
 
   def new
-    @answer_option = @survey.answer_options.new
+    @answer_option = @survey.answer_options.new(value: calculated_answer_option_value)
     authorize! @answer_option
 
     respond_to do |format|
@@ -63,6 +63,10 @@ class Admin::Survey::AnswerOptionsController < AdminController
   end
 
   private
+
+  def calculated_answer_option_value
+    @survey.answer_options.any? ? (@survey.answer_options.maximum(:value).to_i + 1) : 0
+  end
 
   def set_survey
     @survey = Survey.find(params[:survey_id])
