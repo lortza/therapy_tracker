cb952693b4291df6544ef19d138130b5970216f2
Anne Richardson <richardson.ae@gmail.com>
2026-05-08 14:12:49 -0400

    Fix bug for rendering when answer_options don't exist yet

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -4,6 +4,7 @@ module SurveysHelper
   def display_score_range_steps?(survey)
     allowed_to?(:edit?, survey) &&
       survey.questions.any? &&
+      survey.answer_options.presence &&
       survey.answer_options.maximum(:value) > 0
   end
 end
