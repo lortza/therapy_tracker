a509160982691b25d952844c1331117be475057e
Anne Richardson <richardson.ae@gmail.com>
2026-07-28 11:58:51 -0400

    Eager load surveys in responses to improve performance

--- app/controllers/survey/responses_controller.rb
+++ app/controllers/survey/responses_controller.rb
@@ -7,7 +7,7 @@ class Survey::ResponsesController < ApplicationController
   def index
     authorize! Survey::Response, to: :index?
 
-    response_collection = current_user.survey_responses.where(survey: @survey).order(occurred_at: :desc)
+    response_collection = current_user.survey_responses.where(survey: @survey).includes(:survey).order(occurred_at: :desc)
     @survey_responses = Survey::ResponseDecorator.decorate_collection(response_collection)
   end
 
